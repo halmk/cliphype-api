b4c267a727991625ef311a1ffcdb98ff74694c24
halmk <elle2206@i.softbank.jp>
2022-09-03 08:04:51 +0900

    #30 Create: service for streamer entity

---
+++ backend/service/streamer/streamer.go
@@ -0,0 +1,32 @@
+package streamer
+
+import (
+	"github.com/halmk/cliplist-ttv/backend/db"
+	"github.com/halmk/cliplist-ttv/backend/entity"
+)
+
+func Create(name string) (entity.Streamer, error) {
+	db := db.GetDB()
+	var streamer entity.Streamer
+	{
+		streamer.Name = name
+	}
+	if err := db.Create(&streamer).Error; err != nil {
+		return streamer, err
+	}
+
+	return streamer, nil
+}
+
+func GetByName(name string) (entity.Streamer, error) {
+	db := db.GetDB()
+	var streamer entity.Streamer
+	if err := db.Where("name = ?", name).First(&streamer).Error; err != nil {
+		streamer, err = Create(name)
+		if err != nil {
+			return streamer, nil
+		}
+	}
+
+	return streamer, nil
+}
