bbd71c45b4044cdb2434e2f45082b27ecea0777d
halmk <elle2206@i.softbank.jp>
2022-09-03 08:03:07 +0900

    #30 Create: playlist entity

---
+++ backend/entity/playlist.go
@@ -0,0 +1,14 @@
+package entity
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Playlist struct {
+	gorm.Model
+	Title      string
+	StreamerID int
+	Streamer   Streamer
+	CreatorID  int
+	Creator    User
+}
