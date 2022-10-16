5cafadee0dd12497d6ff484e78e9e3d98a2b0386
halmk <elle2206@i.softbank.jp>
2022-10-16 16:40:38 +0000

    #1 Mod: adapt streamer and hype fileds

--- service/autoclip/autoclip.go
+++ service/autoclip/autoclip.go
@@ -5,13 +5,15 @@ import (
 	"github.com/halmk/cliphype-api/entity"
 )
 
-func Create(clip_id string, edit_url string, user entity.User) (entity.AutoClip, error) {
+func Create(clip_id string, edit_url string, user entity.User, streamer string, hype float64) (entity.AutoClip, error) {
 	db := db.GetDB()
 	var auto_clip entity.AutoClip
 	{
 		auto_clip.ClipID = clip_id
 		auto_clip.EditURL = edit_url
 		auto_clip.User = user
+		auto_clip.Streamer = streamer
+		auto_clip.Hype = hype
 	}
 	if err := db.Create(&auto_clip).Error; err != nil {
 		return auto_clip, err
@@ -29,10 +31,17 @@ func GetAll() ([]entity.AutoClip, error) {
 	return auto_clips, nil
 }
 
-func GetArrayByUser(user_id uint) ([]entity.AutoClip, error) {
+func GetArrayBy(user_id *uint, streamer *string) ([]entity.AutoClip, error) {
 	db := db.GetDB()
 	var auto_clips []entity.AutoClip
-	if err := db.Where("user_id = ?", user_id).Find(&auto_clips).Error; err != nil {
+	cond := make(map[string]interface{})
+	if user_id != nil {
+		cond["user_id"] = *user_id
+	}
+	if streamer != nil {
+		cond["streamer"] = *streamer
+	}
+	if err := db.Where(cond).Find(&auto_clips).Error; err != nil {
 		return auto_clips, err
 	}
 	return auto_clips, nil
